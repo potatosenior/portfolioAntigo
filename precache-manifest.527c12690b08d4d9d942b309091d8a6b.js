21385fb645e4d5cbfb949b7f4e7946465a1246f1
unknown <potatosenior6@hotmail.com>
2020-12-11 16:01:40 -0300

    Updates

---
+++ precache-manifest.527c12690b08d4d9d942b309091d8a6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ca33d1d03d10fd9be86779e26e6f44",
+    "url": "/portfolioAntigo/index.html"
+  },
+  {
+    "revision": "fb67fa9413989eeac392",
+    "url": "/portfolioAntigo/static/css/main.ca105167.chunk.css"
+  },
+  {
+    "revision": "854b4ac96fa4d571168b",
+    "url": "/portfolioAntigo/static/js/2.614711ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolioAntigo/static/js/2.614711ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb67fa9413989eeac392",
+    "url": "/portfolioAntigo/static/js/main.9a7a87a2.chunk.js"
+  },
+  {
+    "revision": "d17bb012e78f00665992",
+    "url": "/portfolioAntigo/static/js/runtime-main.2387631b.js"
+  }
+]);
